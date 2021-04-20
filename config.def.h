55b764f93d1e03fa9f8516ad1f4aa51c6d0fdf00
Samir Amer <samirmamer@gmail.com>
2021-04-20 16:48:33 -0400

    Change bg color for selected windows

--- config.def.h
+++ config.def.h
@@ -20,7 +20,7 @@ static const char col_cyan[]        = "#d79921";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray4, col_gray1, col_gray1 },
-	[SchemeSel]  = { col_gray2, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_cyan, col_gray2,  col_cyan  },
 };
 
 /* tagging */
