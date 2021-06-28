0123ee7cb77e44fefb46c6acd944548a2d1d34aa
Samir Amer <samirmamer@gmail.com>
2021-06-28 09:30:58 -0400

    Add some window rules

--- config.def.h
+++ config.def.h
@@ -38,9 +38,13 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class                instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",               NULL,       NULL,       0,            1,           -1 },
+	{ "Brave-browser",      NULL,       NULL,       1 << 1,       0,           -1 },
+	{ NULL,      NULL,       "Task Manager - Brave",       0,       1,           -1 },
+	{ "Lxappearance",        NULL,       NULL,       0,            1,           -1 },
+	{ "Blueman-manager",        NULL,       NULL,       0,            1,           -1 },
+	{ "Pavucontrol",        NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
