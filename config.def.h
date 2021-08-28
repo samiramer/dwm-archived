7b93ff6896124e387807edde29cd34fdb719dad6
Samir Amer <samirmamer@gmail.com>
2021-08-28 12:47:43 -0400

    Make minor config changes

--- config.def.h
+++ config.def.h
@@ -7,7 +7,7 @@ static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows sel
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;     /* 0 means no systray */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
@@ -16,8 +16,8 @@ static const unsigned int gappov    = 10;       /* vert outer gap between window
 static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Noto Sans Mono:size=10" };
-static const char dmenufont[]       = "Noto Sans Mono:size=10";
+static const char *fonts[]          = { "Hack Nerd Font Mono:size=10" };
+static const char dmenufont[]       = "Hack Nerd Font Mono:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
 static char normfgcolor[]           = "#bbbbbb";
@@ -45,6 +45,7 @@ static const Rule rules[] = {
 	{ "Lxappearance",        NULL,       NULL,       0,            1,           -1 },
 	{ "Blueman-manager",        NULL,       NULL,       0,            1,           -1 },
 	{ "Pavucontrol",        NULL,       NULL,       0,            1,           -1 },
+	{ "Galculator",        NULL,       NULL,       0,            1,           -1 },
 };
 
 /* layout(s) */
@@ -74,7 +75,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /* static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray4, "-sb", col_cyan, "-sf", col_gray1, NULL }; */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbordercolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *powercmd[] = { "dmenu_power", NULL };
 static const char *vpncmd[] = { "dmenu_vpn", NULL };
 /* static const char *vpncmd[] = { "dmenu_vpn", col_gray1, col_gray4, col_cyan, dmenufont, NULL }; */
